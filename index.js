9c73544394088d451bea850ae5861640e8cf5659
Kartikey Ranjan <ranjankartikey1999@gmail.com>
2020-10-15 20:07:04 +0530

    updated

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab"); // Hide the current tab:
   // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  x[currentTab].style.display = "none";
+  /*   if (n == 1 && !validateForm()) return false;
+   */ x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
   currentTab = currentTab + n;
   // if you have reached the end of the form... :
