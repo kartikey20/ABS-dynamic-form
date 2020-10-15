71598f44a80c4f6b8ccc11a1116283581b0e0a09
Kartikey Ranjan <ranjankartikey1999@gmail.com>
2020-10-15 14:52:42 +0530

    updated files

--- index.js
+++ index.js
@@ -1,120 +1,121 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
+$("#n_people")
+  .on("change", function () {
+    var noi = $(this).val();
+    var e = $("#tab_1");
+    $(`#tab_1:gt(0)`).remove();
+    //var present = e.length;
+    for (var i = 0; i < noi - 1; i++) {
+      e.clone().insertAfter(e);
+    }
+  })
+  .trigger("change");
+$("#n_people")
+  .on("change", function () {
+    var noi = $(this).val();
+    var e = $("#tab_2");
+    $(`#tab_2:gt(0)`).remove();
+    //var present = e.length;
+    for (var i = 0; i < noi - 1; i++) {
+      e.clone().insertAfter(e);
+    }
+  })
+  .trigger("change");
+$("#n_people")
+  .on("change", function () {
+    var noi = $(this).val();
+    var e = $("#tab_3");
+    $(`#tab_3:gt(0)`).remove();
+    //var present = e.length;
+    for (var i = 0; i < noi - 1; i++) {
+      e.clone().insertAfter(e);
+    }
+  })
+  .trigger("change");
 showTab(currentTab);
-$(document).ready(function() {
-    $("input[name$='group']").click(function() {
-        var test = $(this).val();
 
-        $("#electric").show();
-    });
-});
 //clone tabs according to number of people
-
-$('#n_people').on('change', function () {
-    var noi = $(this).val();
-    var e = $(`#tab3`);
-    $(`#tab3:gt(0)`).remove();
-    //var present = e.length;
-    for (var i = 0; i < (noi - 1); i++) {
-        e.clone().insertAfter(e);
-    }
-}).trigger('change');
-$('#n_people').on('change', function () {
-    var noi = $(this).val();
-    var e = $(`#tab2`);
-    $(`#tab2:gt(0)`).remove();
-    //var present = e.length;
-    for (var i = 0; i < (noi - 1); i++) {e.clone().insertAfter(e);
-    }
-}).trigger('change');
-$('#n_people').on('change', function () {
-    var noi = $(this).val();
-    var e = $(`#tab1`);
-    $(`#tab1:gt(0)`).remove();
-    //var present = e.length;
-    for (var i = 0; i < (noi - 1); i++) {
-        e.clone().insertAfter(e);
-    }
-}).trigger('change');
 
 // Display the current tab
 
 function showTab(n) {
-
-    // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    // ... and fix the Previous/Next buttons:
-    if (n === 0) {
-        document.getElementById("prevBtn").style.display = "none";
-    } else {
-        document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n === (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = "Submit";
-    } else {
-        document.getElementById("nextBtn").innerHTML = "Next";
-    }
-    // ... and run a function that displays the correct step indicator:
-    fixStepIndicator(n)
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n === 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n === x.length - 1) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n);
 }
 
 function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  /*if (n == 1 && !validateForm()) return false;*/
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
 
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-/*
-    if (n == 1 && !validateForm()) return false;
-*/
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
 
-    // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
-        //...the form gets submitted:
-        document.getElementById("regForm").submit();
-        return false;
-    }
-
-    // Otherwise, display the correct tab:
-    showTab(currentTab);
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
 }
 
 function validateForm() {
-    // This function deals with validation of the form fields
-    var x, y, i, n, valid = true;
-    n = document.getElementById('n_people').value;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    n,
+    valid = true;
+  n = document.getElementById("n_people").value;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
 
-    if (isNaN(n)) {
-        return false;
+  if (isNaN(n)) {
+    return false;
+  }
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
     }
-    // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
-        // If a field is empty...
-        if (y[i].value == "") {
-            // add an "invalid" class to the field:
-            y[i].className += " invalid";
-            // and set the current valid status to false:
-            valid = false;
-        }
-    }
-    // If the valid status is true, mark the step as finished and valid:
-    if (valid) {
-        document.getElementsByClassName("step")[currentTab].className += " finish";
-    }
-    return valid; // return the valid status
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
 }
 
 function fixStepIndicator(n) {
-    // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
-        x[i].className = x[i].className.replace(" active", "");
-    }
-    //... and adds the "active" class to the current step:
-    x[n].className += " active";
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
 }
